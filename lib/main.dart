3976a0290bce90a5f80a7e3165cc3afe0bf7560e
dilermando.augusto <Dilermando.Augusto-ext@ab-Inbev.com>
2023-09-18 14:32:24 -0300

    Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: const LoginPage());
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white
+      ),
+      home: LoginPage(),
+    );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  final _tUser = TextEditingController();
+  final _tPwd = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +20,50 @@ class LoginPage extends StatelessWidget {
 
   _body() {
     return Container(
-      color: Colors.white,
+      padding: const EdgeInsets.all(16),
+      child: ListView(
+        children: [
+          _textFormField("Username", "Type your Login name",
+              controller: _tUser),
+          const SizedBox(
+            height: 10,
+          ),
+          _textFormField("Password", "Type your Password",
+              password: true, controller: _tPwd),
+          const SizedBox(
+            height: 20,
+          ),
+          _elevatedButton("Login", () => _onClickLogin())
+        ],
+      ),
     );
+  }
+
+  _elevatedButton(String label, onPressed) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue,
+        padding: const EdgeInsets.all(12),
+      ),
+      child: Text(label),
+    );
+  }
+
+  _textFormField(String label, String hint, {password = false, controller}) {
+    return TextFormField(
+      controller: controller,
+      obscureText: password,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+      ),
+    );
+  }
+
+  _onClickLogin() {
+    String user = _tUser.text;
+    String pwd = _tPwd.text;
+    print("$user ; $pwd");
   }
 }
