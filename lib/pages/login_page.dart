cc94351261f7a275e312d84ee08c8ec9573cdd34
dilermando.augusto <Dilermando.Augusto-ext@ab-Inbev.com>
2023-10-12 10:29:58 -0300

    86. TextInputType / Action

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
 
   final _formKey = GlobalKey<FormState>();
   final _tUser = TextEditingController(text: "diler");
   final _tPwd = TextEditingController(text: "1234");
+  final _focusPwd = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +43,7 @@ class LoginPage extends StatelessWidget {
               "Type your Login name",
               _validateUser,
               _tUser,
+              nextFocus: _focusPwd,
             ),
             const SizedBox(
               height: 10,
@@ -41,6 +54,9 @@ class LoginPage extends StatelessWidget {
               _validatePwd,
               _tPwd,
               password: true,
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.done,
+              focusNode: _focusPwd,
             ),
             const SizedBox(
               height: 20,
@@ -80,13 +96,28 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  _textFormField(String label, String hint,
-      FormFieldValidator<String?> validator, TextEditingController controller,
-      {password = false}) {
+  _textFormField(String label,
+      String hint,
+      FormFieldValidator<String?> validator,
+      TextEditingController controller, {
+        password = false,
+        TextInputType keyboardType = TextInputType.text,
+        TextInputAction textInputAction = TextInputAction.next,
+        FocusNode? focusNode,
+        FocusNode? nextFocus,
+      }) {
     return TextFormField(
       controller: controller,
       obscureText: password,
       validator: validator,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
+      onFieldSubmitted: (String text) {
+        if (nextFocus != null) {
+          FocusScope.of(this.context).requestFocus(_focusPwd);
+        }
+      },
       decoration: InputDecoration(
         labelText: label,
         hintText: hint,
