e477819393be451247ebf21d656986c44b9d2be8
dilermando.augusto <Dilermando.Augusto-ext@ab-Inbev.com>
2023-11-02 11:29:28 -0300

    96. api response

---
+++ lib/utils/alert.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+alert(BuildContext context, String title, String msg) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) {
+      return WillPopScope(
+        onWillPop: () async => false,
+        child: AlertDialog(
+          title: Text(title),
+          content: Text(msg),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      );
+    },
+  );
+}
